6a9213723ac76859da646d2e264156263c145949
rorywalsh <rorywalsh@ear.ie>
2019-11-14 17:02:25 +0000

    starting cmake build

---
+++ cpp/build/CMakeFiles/GetChannelExample,.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/walshr/sourcecode/csoundAPI_examples/cpp/SetChannelExample.cpp" "/Users/walshr/sourcecode/csoundAPI_examples/cpp/build/CMakeFiles/GetChannelExample,.dir/SetChannelExample.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "Clang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/Library/Frameworks/CsoundLib64.framework/Versions/6.0/Headers"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cpp/build/CMakeFiles/GetChannelExample.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/GetChannelExample.dir/SetChannelExample.cpp.o"
+  "GetChannelExample.pdb"
+  "GetChannelExample"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/GetChannelExample.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
