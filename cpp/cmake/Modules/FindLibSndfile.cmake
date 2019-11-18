55aa646daf234f80fc5f7fd165b4d90124b2e493
rorywalsh <rorywalsh@ear.ie>
2019-11-18 19:48:13 +0000

    tifying up code

--- cpp/cmake/Modules/FindLibSndfile.cmake
+++ cpp/cmake/Modules/FindLibSndfile.cmake
@@ -1,15 +1,13 @@
 # Try to find the libsndfile library.
 # Once done this will define:
-#  LIBSNDFILE - System has the libsndfile library
+#  LIBSNDFILE_FOUND - System has the libsndfile library
 #  LIBSNDFILE_INCLUDE_DIR - The libsndfile include directories.
 #  LIBSNDFILE_LIBRARIES - The libraries needed to use the libsndfile library.
 
 if(APPLE)
-  find_path(LIBSNDFILE_INCLUDE_DIR libsndfile.h HINTS /usr/local/Cellar/libsndfile/1.0.28/include
-  "/usr/local/Cellar/libsndfile/1.0.28/include")
+  find_path(LIBSNDFILE_INCLUDE_DIR libsndfile.h HINTS /usr/local/Cellar/libsndfile/1.0.28/include "/usr/local/Cellar/libsndfile/1.0.28/include")
 elseif(WIN32)
-  find_path(LIBSNDFILE_INCLUDE_DIR sndfile.h
-            HINTS "C:\\Program Files\\Mega-Nerd\\libsndfile\\include")
+  find_path(LIBSNDFILE_INCLUDE_DIR sndfile.h HINTS "C:\\Program Files\\Mega-Nerd\\libsndfile\\include")
 else()
   find_path(LIBSNDFILE_INCLUDE_DIR libsndfile.h PATH_SUFFIXES libsndfile)
 endif()
@@ -25,8 +23,7 @@ endif()
 include(FindPackageHandleStandardArgs)
 # # handle the QUIETLY and REQUIRED arguments and set LIBSNDFILE_FOUND to TRUE
 # # if all listed variables are TRUE
-find_package_handle_standard_args(LIBSNDFILE
-                                  LIBSNDFILE_LIBRARY LIBSNDFILE_INCLUDE_DIR)
+find_package_handle_standard_args(LIBSNDFILE LIBSNDFILE_LIBRARY LIBSNDFILE_INCLUDE_DIR)
 mark_as_advanced(LIBSNDFILE_INCLUDE_DIR LIBSNDFILE_LIBRARY)
 
 set(LIBSNDFILE_INCLUDE_DIRS ${LIBSNDFILE_INCLUDE_DIR})
